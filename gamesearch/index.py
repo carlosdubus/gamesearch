3aa6bdf529c7f7e8b742a3c94558823176bdfd13
Carlos Dubus <carlos.garciadm@gmail.com>
2016-04-30 16:46:58 -0400

    add file based inverted index

---
+++ gamesearch/index.py
@@ -0,0 +1,38 @@
+import re
+import json
+import os.path
+
+
+class InvertedIndex:
+    def __init__(self, path=None):
+        self.path = path
+
+    def tokenize(self, text):
+        return [s.lower() for s in re.split('\W+', text) if len(s) >= 2]
+
+    def query(self, query):
+        sets = []
+        for token in self.tokenize(query):
+            docs = list(self.read_token(token))
+            sets.append(set([d[0] for d in docs]))
+        return set.intersection(*sets)
+
+    def write_token(self, token, hit):
+        if not token:
+            return
+        with open(self.path + "/" + token, "a") as fp:
+            fp.write("%s,%s,\n" % hit)
+
+    def read_token(self, token):
+        token_path = self.path + "/" + token
+        if not os.path.isfile(token_path):
+            return
+        with open(token_path, "r") as fp:
+            for line in fp:
+                r = line.split(",")
+                yield (r[0], r[1])
+
+    def add_document(self, text, doc_id):
+        tokens = self.tokenize(text)
+        for pos, token in enumerate(tokens):
+            self.write_token(token, (doc_id, pos))
